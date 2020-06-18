6967d1e436638e01545a19fae9783b5a8ae89d19
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-18 16:57:53 -0400

    add all_links resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # Queries are just represented as fields
+    # "all_links" will be camelCased to "allLinks"
+    field :all_links, [LinkType], null: false
+
+    # This method is invoked whenever an "all_links" field is being resolved
+    def all_links
+      Link.all
     end
   end
 end
